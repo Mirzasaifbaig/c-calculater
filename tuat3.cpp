9c4a15d52fa32be50b68dde7f432dbd655672630
Mirzasaifbaig <98511847+Mirzasaifbaig@users.noreply.github.com>
2023-11-28 01:15:43 +0500

    Add files via upload

---
+++ tuat3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+int glo =3;
+void sum(){
+    int a;
+}
+int main(){
+    int glo =23;
+    // int a = 14;
+    // int b = 15;
+    glo=67;
+    
+   int a=4 ,  b=12;
+   float pi=3.14;
+   char c='u';
+   bool issaif = false;
+   sum();
+//  cout<<"\n The value of u is:"<<c; 
+//      cout<<"\n This is saif baig.\n Here the value of a is "<<a<<".\n The value of b is "<<b;
+//     cout<< "\n The value of pi is"<<pi;
+    cout<< glo << issaif;
+    
+return 0;
+}
